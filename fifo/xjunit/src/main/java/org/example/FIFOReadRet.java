9a2abc28d263b87d5f0531907532f0def3bbae62
cjh <211250069@smail.nju.edu.cn>
2025-04-08 15:22:11 +0800

    fix record feature in j17

--- fifo/xjunit/src/main/java/org/example/FIFOReadRet.java
+++ fifo/xjunit/src/main/java/org/example/FIFOReadRet.java
@@ -1,3 +1,26 @@
 package org.example;
 
-public record FIFOReadRet(int rdata, int empty, int full){ };
+public class FIFOReadRet{
+    int rdata;
+    int empty;
+    int full;
+
+    FIFOReadRet(){
+
+    }
+
+    FIFOReadRet(int rdata, int empty, int full){
+        this.rdata = rdata;
+        this.empty = empty;
+        this.full = full;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof FIFOReadRet) {
+            FIFOReadRet other = (FIFOReadRet) obj;
+            return rdata == other.rdata && empty == other.empty && full == other.full;
+        }
+        return false;
+    }
+};
--- fifo/xjunit/src/main/java/org/example/FIFOWriteRet.java
+++ fifo/xjunit/src/main/java/org/example/FIFOWriteRet.java
@@ -1,3 +1,24 @@
 package org.example;
 
-public record FIFOWriteRet(int empty, int full) { };
+
+public class FIFOWriteRet {
+    int empty;
+    int full;
+
+    FIFOWriteRet() {
+    }
+
+    FIFOWriteRet(int empty, int full) {
+        this.empty = empty;
+        this.full = full;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof FIFOWriteRet) {
+            FIFOWriteRet other = (FIFOWriteRet) obj;
+            return empty == other.empty && full == other.full;
+        }
+        return false;
+    }
+}
